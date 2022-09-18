321fe9ac1115fc3dfb6047218275ac61d82002ce
Lavishq <v.whitehearted@simplelogin.co>
2022-09-18 14:40:03 +0530

    can be added extension by loadin out/

--- frontend/pages/Extension/index.js
+++ frontend/pages/Extension/index.js
@@ -38,7 +38,8 @@ const Index = () => {
 
     // If user is not connected to the Mumbai network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 137) {
+    console.log(chainId);
+    if (chainId !== 80001) {
       window.alert("Change the network to Mumbai");
       throw new Error("Change network to Mumbai");
     }
